0cb88a0f28c526297435839b998f522ae058f227
Carhuacusma <51172263+Carhuacusma@users.noreply.github.com>
2019-09-24 19:03:51 -0500

    Add files via upload

--- Algoritmo_Dante.py
+++ Algoritmo_Dante.py
@@ -54,25 +54,39 @@ def algoritmoDante(size, arrRec):
     n = len(arrRec)
     quickSort(arrRec, 0, n-1)
     # arrRec está ordenado de mayor a menor area
+    
+    #------definir el minimo desecho o cuantas planchas usar------
     areaT = size[0] * size[1]
     bestWaste = sumArea(arrRec)
-    arrRes = []
+    nPlanchas = 1
+    if areaT > bestWaste:
+        bestWaste = sizeArea - bestWaste
+    else:
+        nPlanchas = bestWaste//sizeArea #para que sea entero //
+    #-------------------------------------------------------------
+    
+    arrRes = []*n
     # [[(pos.x, pos.y), (ancho_x, largo_y)], [(pos2.x,pos2.y),(ancho2,largo2)], ...]
     girado = [False]*n
     # girado ordenado según los ids
-    def paso(rec, j, forma):
-        # recibe uno de los recortes, la "iteracion" del paso, y la forma que está cortada
-        arrAreaCut = []
-        for aux in forma:
-            arrAreaCut.append(aux[1])
-        sumAreaCut = sumArea(arrAreaCut)
-        if sumAreaCut == bestWaste:
-            ##si es el mejor caso, regresaf
+    def paso(rec, j, nP, forma):
+        # recibe uno de los recortes, la "iteracion" del paso, 
+        # el numero de Plancha en que trabaja, y la forma que está cortada
+        if nPlanchas == 1:
+            arrAreaCut = []
+            for aux in forma:
+                arrAreaCut.append(aux[1])
+            sumAreaCut = sumArea(arrAreaCut)
+            if sumAreaCut == bestWaste:
+                return forma
+        elif nP == nPlanchas and len(forma) == n:
             return forma
+        
         ##posicionar rec en forma:
+        
         # TO DO ---------------------------
         # paso(arrRec[j+1], j + 1, forma)
-        ##girar si es necesario
+        ##--->girar si es necesario
         
     forma = []
     ##empezar con el recorte de mayor area en la esquina
@@ -87,10 +101,13 @@ for ej in ejemplo:
 print(sumArea(ejemplo))
 
 
-# In[3]:
+# In[9]:
 
 
-
+arr = []*5
+arr.insert(1,3)
+arr.insert(2,([(1,2),(2,3)]))
+print(arr)
 
 
 # In[ ]:
