20fa4f14438d369e804f2f602d7b649cf175e731
NataliaMelissa <50978032+NataliaMelissa@users.noreply.github.com>
2019-09-25 00:45:04 -0500

    Cálculo del desperdicio y avance con el algoritmo

--- AlgoritmoNatalia.py
+++ AlgoritmoNatalia.py
@@ -6,40 +6,79 @@
 
 def Natalia(largo, ancho, arrRec, n):
     
-    for i in range(largo):
-        for j in range(ancho):
-            placa[i][j] = 0
+    ##1. Crear e inicializar matriz
+    placa = InicializarMatriz(ancho, largo)
     
-    arrAreas = [] ##area de los recortes
-    arrIdRecortes = [] ##para identificar los recortes utilizados en la placa
+    ##2. Crear variables necesarias
     AreaPlaca = largo * ancho
+    AreaRecortes = 0
     ContRecortes = 0
-        
+    PlacaSR = True ##PlacaSR = Placa sin recortes
+    n = len(arrRec)
+    
     for i in range(n):
-        area = arrRec[0][1] * arrRec[1][0]
-        arrAreas.append(area)
+        AreaRecortes = arrRec[0][1] * arrRec[1][0]
+
+        
+    DesperdicioOptimo = AreaPlaca - AreaRecortes
+        
+    ##3. Ordenar arreglo de recortes por sus anchos
+    sort(arrRec[0][1][0][0])
     
-    ##Ordenar el arreglo de areas
-    
-    ##Ordenar arreglo de recortes
-    
-    ##Sumar el mayor de los recortes con el segundo mayor y comparar con el ancho de la placa
+    ##4. Recortar placa
     for i in range(n):
         if arrRec[i] + arrRec[i + 1] == ancho:
-            mayor = 0
+            MayorAncho = 0
             
             if arrRec[i][0] > arr[i + 1]:
-                mayor = arr[i][0]
+                MayorAncho = arr[i][0]
             
             else:
-                mayor = arr[i + 1][0]
+                MayorAncho = arr[i + 1][0]
+            
+            if PlacaSR == True:
+                for i in range(largo):
+                    for j in range(MayorAncho):
+                        placa[i][j] = 1
+                PlacaSR = False
                 
-            for i in range(largo):
-                for j in range(mayor):
-                    placa[i][j] = 1
+            else:
+                for i in range(largo):
+                    for j in range(mayor):
+                        if placa[i][j] == 0:
+                            placa == 1
             
         
         else:
             ##Buscar suma más óptima
-            LlenarMatriz()
+    
+    Desperdicio = 0
+    do{
+    if DesperdicioOptimo < 0:
+        Placas.append(InicializarMatriz(largo, ancho))
+        ##Realizar los recortes en la siguiente placa
+        
+        ##Calcular el desperdicio
+        for i in range(largo):
+            for j in range(ancho):
+                if placa[i][j] == 0:
+                    desperdicio += 1
+    
+    else:
+        break
+    }while(Desperdicio >= 0)
+    
+    ##5. Calcular el desperdicio                
+    DespPorcentaje = (desperdicio/AreaPlaca)*100
+    
+    ##6. Mostrar Resultados
+    print("El desperdicio generado es: "+ desperdicio + " en metros")
+    print("El desperdicio generado es: "+ DespPorcentaje + "%")
+    print("Se utilizaron "+ Placas + " planchas")
+    
+ def InicializarMatriz(ancho, largo):
+    for i in range(largo):
+        for j in range(ancho):
+            placa = [i][j]
+    return placa
 
