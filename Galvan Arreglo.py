1a9f4cfc437b1f83d1c9e1318cd8bbca30e15118
iller15 <39936760+iller15@users.noreply.github.com>
2019-09-25 12:11:46 -0500

    graph

--- Galvan Arreglo.py
+++ Galvan Arreglo.py
@@ -1,43 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Variable universal arreglo de rectangulos + dimensiones de la plancha
+import matplotlib.pylab as plt
+import random as rnd
+class Rectangulo:
+    def __init__(self,a,l,en,i):
+        self.base = l
+        self.alto = a
+        self.n = en
+        self.ids = i
+ArrRect = [Rectangulo(rnd.randint(0,15),rnd.randint(0,30),rnd.randint(0,10),i) for i in range(5)] 
 
-# In[35]:
+altura = 15
+base = 30
+arr=[[i+1 for i in range(base)] for _ in range(altura)]#matriz plancha
 
+#Probaremos con esto ArrRect[0]
+for i in range(ArrRect[0].alto):
+    arr[i] = arr[i][ArrRect[0].base:]
 
-def lenarr(arr):
-    arrlen = []
-    for x in range(len(arr)):
-        arrlen.append(len(arr[x]))
-    return arrlen
-    
-arr=[[1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10],
-
-    ]
-rect = (5,2)
-for i in range(rect[0]):
-    arr[i] = arr[i][rect[1]:]
 print(arr)
-print("                               ")
+print("")
 
 
 rect = (6,2)
 for i in range(rect[0]-1):
-    if rect[0] <= len(arr[i]):
-        if len(arr) >= i+1 and arr[i+1][0] != arr[i][0]:
+    if ArrRect[0].base <= len(arr[i]):
+        if len(arr) >= i+1 == True and arr[i+1][0] != arr[i][0]:
             break
         else:
             arr[i] = arr[i][rect[1]:]
 
-        
-print(arr)
+            
+            
+for i in range(len(ArrRect)):
+    print("Rectangulo ",ArrRect[i].ids,":",ArrRect[i].base,ArrRect[i].alto,ArrRect[i].n)
 
-    
+
     
     
 
